bd7b5178e7670b3a3112483b55405c1ec1b741e8
Arthur_Ferreira <arthurcarvalhof@gmail.com>
2026-01-28 11:10:19 -0300

    adicionado autenticacao

--- api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,4 +1,24 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAutenticacao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager autenticacaoManager;
+
+    @PostMapping("/login")
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = autenticacaoManager.authenticate(authenticationToken);
+        return ResponseEntity.ok().build();
+    }
 }
